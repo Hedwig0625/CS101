3e5fa2570f1f683982cf9e190aeed697826f6452
Hedwig0625 <hedwigchen060625@gmail.com>
2024-12-05 15:26:42 +0800

    Create main2.c

---
+++ EX3/main2.c
@@ -0,0 +1,17 @@
+#include <studio.h>
+int main(){
+  int number = 75
+  if (number % 15 ==0){
+    printf("Love IU");
+  }
+  else if (number % 5 ==0){
+    printf("IU");
+  }
+  else if (number % 3 ==0){
+    printf("Love");
+  }
+  else {
+    printf("%d,number");
+  }
+  return 0
+}
