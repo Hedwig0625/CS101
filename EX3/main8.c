f2a7b38252bbebf90fbce1d99bfdf9bb9e3f745a
Hedwig0625 <hedwigchen060625@gmail.com>
2024-12-05 17:49:38 +0800

    Create main8.c

---
+++ EX3/main8.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+  int x=3
+  int y=5
+  int z=4
+  if (x>0){
+    int hundred=x;
+  }
+  else{
+    int hundred=x*(-1)
+  }
+  if (y>0){
+    int ten=y;
+  }
+  else{
+    int ten=y*(-1);
+  if (z>0){
+    int one=z;
+  }
+  else{
+    int one=z*(-1);
+  printf("%d",hundred+ten+one)
+  return 0;
+}
