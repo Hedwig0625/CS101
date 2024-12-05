4af2bf58bc530b1b21a736f50e8301a6c4f4e37b
Hedwig0625 <hedwigchen060625@gmail.com>
2024-12-05 15:49:57 +0800

    Create main5.c

---
+++ EX3/main5.c
@@ -0,0 +1,17 @@
+#include <studio.h>
+int main(){
+  int min=43234
+  if (min <=30){
+    printf("free to go");
+  }
+  else if (min <=0){
+    printf("時間為輸入或未停車")
+  }
+  else if (min >=240){
+    printf("$240");
+  }
+  else{
+    
+    }
+  }
+}
