c5a3cdcf4063ff61b2047e03770f13f2928eb8d6
Hedwig0625 <hedwigchen060625@gmail.com>
2024-12-10 17:29:55 +0800

    Update main3.c

--- hw5/main3.c
+++ hw5/main3.c
@@ -5,20 +5,20 @@ int main(){
     for(int x=1;x <=9;x++){
       printf("%d*%d=％d\t",x,y,x*y);//\t是右移一點
     }
-  printf("/n")
+  printf("\n")
   }
   return 0;
 }
 ///////////////作業
 #include <stdio.h>
 int main(){ 
-  for(int x=1, y=1; x <10 , y <10; x++,y++){
-    printf("%d*%d=％d\t",x,y,x*y");
-    if (x=9){
-      printf("/n");
+  for(int x=1, y=1; x <10 && y <10; x++,y++){//要用＆＆要不然會視後面那項為條件，忽略前項
+    printf("%d*%d=％d\t",x,y,x*y);
+    if (x == 9){//記得要用==，要不然會變指派
+      printf("\n");
     }
     else{
-      continue;
+      continue;//其實可以不用else,是多餘ㄉ
     }
   }
   return 0;
