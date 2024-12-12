8cfe5c9ef2f71cc559e98ebf4b6cf81ac23552e1
Hedwig0625 <hedwigchen060625@gmail.com>
2024-12-12 15:19:21 +0800

    Update main3.c

--- EX4/main3.c
+++ EX4/main3.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
 int main(){
-  for(int i =1,){}
+  for(int x =1,y=1;x<10 && y<10;y++){
+    printf("%d*%d=%d",x,y,x*y);
+    if(y == 9){
+      printf("\n");
+      x=++;
+      y=0;
+    }
+    else{
+      continue;
+    }
+  }
+  return 0;
 }
