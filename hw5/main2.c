26fa84faef994856c6e5c1f4989d16110c2a6a18
Hedwig0625 <hedwigchen060625@gmail.com>
2024-12-12 14:25:20 +0800

    Update main2.c

--- hw5/main2.c
+++ hw5/main2.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(){ 
-  int n=10000;//算幾次
+  int n=1000000000;//算幾次
   double pi=0.0;//pi的累加結果
   int sign=1;//控制正負
   
@@ -11,3 +11,28 @@ int main(){
   printf("PI=%.5f\n",pi);
   return 0;
 }
+
+
+
+/////老師ver
+double pi=4.0
+int flag=1
+long ipi=0
+int x =0
+
+for(int i =3,i<1000000;i+=2){////第一項是除一，免算，直接從3開始
+  if (flag == 0){////flag控制正負,1負0正
+    pi+=(4.0/i);
+    flag++;
+  }
+  else{
+    pi -=(4.0)/i;
+    flag--;
+  }
+  ipi = pi* 1000000;
+  if(ipi == 314159){
+    x=i;
+    break;
+  }
+}
+printf("%d.%.5f",x,pi);
