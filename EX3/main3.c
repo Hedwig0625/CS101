92d931785d62b20c679d0d53c8d635a2c972ab14
Hedwig0625 <hedwigchen060625@gmail.com>
2024-12-12 13:33:00 +0800

    Update main3.c

--- EX3/main3.c
+++ EX3/main3.c
@@ -10,3 +10,14 @@ else{
 }
 return 0;
 }
+
+
+
+//////老師ver
+if(i & 1){////如果輸入3,0011 and 0001,輸出為10,為true,if 成立
+  ////////如果輸入8,1000 and 0001輸出0000,為false,else成立
+  prinf("false");
+}
+else{
+  printf("true");
+}
