9faee2b7c8ac7a5453372ae49c7e618e5a668e8b
Hedwig0625 <hedwigchen060625@gmail.com>
2024-12-12 13:26:17 +0800

    Update main1.c

--- EX3/main1.c
+++ EX3/main1.c
@@ -10,3 +10,11 @@ int main() {
   }
   return 0;
 }
+////////老師ver
+if(i & (i-1)){/////如果輸入8,1000 and 0111出來是0000,判斷為0,跳到else
+  /////如果輸入9,1001 and 1000出來是1000,if 成立
+  printf("false");
+}
+else{
+  printf("true");
+}
