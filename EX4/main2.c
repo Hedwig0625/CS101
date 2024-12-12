d69fc22915ecd6a86c42462c72a13276dcec6400
Hedwig0625 <hedwigchen060625@gmail.com>
2024-12-12 15:10:43 +0800

    Create main2.c

---
+++ EX4/main2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+  double i=4.0
+  int sign=0
+  long ipi=0
+  int x=0
+  for(int i=3,i<10000000,i+=2){
+    if (sign ==0){
+    sign++;
+    pi-=(4.0)/i;
+    }
+    else{
+      sing--;/////為正數
+      pi +=(4.0)/i;
+    }
+  ipi= pi *1000000
+  if(ipi=314159){
+    x=pi;
+    break;
+  }
+  }
+printf("%d.%5f",x,pi);
+return 0;
+}
