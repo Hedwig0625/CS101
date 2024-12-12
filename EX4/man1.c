daad5cd9808743b016e546d223e8c2b3dd291383
Hedwig0625 <hedwigchen060625@gmail.com>
2024-12-12 14:55:14 +0800

    Create man1.c

---
+++ EX4/man1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+  int n=7;
+  for(int i=1;i <=n;i++){
+    for(j=i,j<i-1,j--){
+      printf(" ");
+      if(j=i-1){
+      printf("%d",i);
+      }
+      else{
+      
+      }
+    }
+  printf("\n");
+  }
+  return 0;
+}
