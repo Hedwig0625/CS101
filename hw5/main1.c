0b4c83ea4255ed875a4a75a129b21634efa69133
Hedwig0625 <hedwigchen060625@gmail.com>
2024-12-15 17:28:24 +0800

    Update main1.c

--- hw5/main1.c
+++ hw5/main1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(){
-  int n=7//七層
+  int n=7;//七層
   for(int i=1;i<=n;i++){//從1開始,最多到7(控制行,最下面的printf)
     for(int j=1;j<=n;j++){//控制印數字（else)
       if(j<=n-i){//如果印的數字到n-1(因為前面應該要有n-1個空個）,則需換行
